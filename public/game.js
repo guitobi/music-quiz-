f3938d17cec722ea3deb3b02675e5d85f03cc856
guitobi <skullvisit@gmail.com>
2025-10-29 00:41:55 +0200

    Rooms created

--- public/game.js
+++ public/game.js
@@ -1,20 +1,92 @@
 const socket = io(); 
 
+let currentRoom = null;
+
 socket.emit('hello', 'Hello from client');
 socket.on('response', data => console.log(data));
 
-const btnEl = document.querySelector('.btn');
+const messageSendBtnEl = document.querySelector('.btn');
 const inputEl = document.querySelector('#messageInput');
-const divEl = document.querySelector('.message');
+const chatDivEl = document.querySelector('.message');
+const createRoomBtn = document.querySelector('.createRoom');
+const joinRoomBtn = document.querySelector('.joinRoom');
+const nameInputEl = document.querySelector('.nameInput');
+const errorDiv = document.querySelector('.error-div');
+const roomInfo = document.querySelector('.roomInfo');
+const roomCodeInput = document.querySelector('.roomCode');
 
-btnEl.addEventListener('click', () => {
-    socket.emit('message', inputEl.value);
+socket.on('message', (data) => {
+    console.log('Отримав повідомлення:', data);
+    const p = document.createElement('p');
+    p.textContent = data;
+    chatDivEl.appendChild(p);
+});
+
+socket.on('room-joined', (roomCode) => {
+    currentRoom = roomCode;
+    roomInfo.textContent = `Приєднався до: ${roomCode}`;
+    nameInputEl.value = '';
+    roomCodeInput.value = '';
+});
+
+socket.on('room-created', (roomCode) => {
+    currentRoom = roomCode;
+    roomInfo.textContent = `Твоя кімната: ${roomCode}`;
+    nameInputEl.value = '';
+});
+
+
+const createRoom = () => {
+    errorDiv.innerHTML =  '';
+    const userNickname = nameInputEl.value.trim();
+    if(userNickname === '') {
+        const errorP = document.createElement('p');
+        errorP.textContent = 'Enter a valid nickname';
+        errorDiv.appendChild(errorP);
+        return;
+    } else {
+        socket.emit('create-room', userNickname);
+    }
+};
+
+const joinRoom = () => {
+    console.log(`Checking Nickname: "${nameInputEl.value}", Room Code: "${roomCodeInput.value}"`);
+    errorDiv.innerHTML =  '';
+    const userNickname = nameInputEl.value.trim();
+    const roomCode = roomCodeInput.value.trim();
+    if(userNickname === '' || roomCode === '') {
+        const errorP = document.createElement('p');
+        errorP.textContent = 'Enter a valid nickname or room code!';
+        errorDiv.appendChild(errorP);
+        return;
+    } else {
+        socket.emit('join-room', {userNickname, roomCode});
+        currentRoom = roomCode;
+        roomCodeInput.value = '';
+        nameInputEl.value = '';
+    }
+    
+};
+
+messageSendBtnEl.addEventListener('click', () => {
+        if (!currentRoom) {
+        alert('Спочатку створи або приєднайся до кімнати!');
+        return;
+    }
+    
+    socket.emit('message', {
+        roomCode: currentRoom,
+        text: inputEl.value
+    });
     inputEl.value = '';
 });
 
-socket.on('message', data => {
-    console.log(data);
-    const p = document.createElement('p');
-    p.textContent = data;
-    divEl.appendChild(p);
-});
+createRoomBtn.addEventListener('click', createRoom);
+joinRoomBtn.addEventListener('click', joinRoom);
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -13,10 +13,20 @@ app.use(express.static('public'))
 
 io.on('connection', socket => {
     console.log('User is connected');
-    socket.on('message' , (data) => {
-        socket.broadcast.emit('message', data);
-    })
+    socket.on('message' , ({roomCode, text}) => {
+       io.to(roomCode).emit('message', text);
+    });
+    socket.on('create-room', playerName => {
+        const roomCode = Math.random().toString(36).substring(2, 8).toUpperCase();
+        socket.join(roomCode);
+        socket.emit('room-created', roomCode);
+    });
+    socket.on('join-room', ({name, roomCode}) => {
+        socket.join(roomCode);
+        socket.emit('room-joined', roomCode);
+    });
 });
+
 
 server.listen(port, () => {
     console.log(`Server started on port ${port}`)
