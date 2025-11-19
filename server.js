c6d568bc15c5b8e98f9ca93e049e1a42350d0ae1
guitobi <118008624+guitobi@users.noreply.github.com>
2025-11-19 10:54:31 +0200

    Change default port from 3030 to 3000

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const SpotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET
 });
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3000;
  
 app.use(express.static('public'));
 app.use(express.json());
